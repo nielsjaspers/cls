6720fbd457e1b5705ed17f8604978d4a502951b0
Niels Jaspers <naeliss.github@gmail.com>
2024-12-18 14:21:13 +0100

    feat: 'secrets' config
    generalized server and client config in secrets.go config file

---
+++ secrets/secrets.go
@@ -0,0 +1,9 @@
+package secrets
+
+var ServerCrtPath string = "./server-crt/server.crt"
+var ServerKeyPath string = "./server-crt/server.key"
+
+var CertAuthPath string = "./server-crt/ca.crt"
+
+const URL string = "https://localhost:443"
+var ServerURL string = "localhost:443"
